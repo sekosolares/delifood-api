f63fbb873b80378d5c23534e1d27c9be2ee282af
Axel J. Solares <axel.solares.g@gmail.com>
2023-07-20 16:40:19 -0600

    [Update]: remove unused import on route

--- src/users/user.routes.ts
+++ src/users/user.routes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { IUnitUser, IUser } from './user.interface';
+import { IUnitUser } from './user.interface';
 import { StatusCodes } from 'http-status-codes';
 import * as database from './user.database';
 
